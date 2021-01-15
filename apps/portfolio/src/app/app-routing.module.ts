317540028d10fb493dbb1abe73b6b8e9a786fc94
E-jarod <ejilane.jarod@gmail.com>
2021-01-15 11:56:05 +0100

    remove unused imports

--- apps/portfolio/src/app/app-routing.module.ts
+++ apps/portfolio/src/app/app-routing.module.ts
@@ -1,11 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { TechWatchComponent } from './tech-watch/tech-watch.component';
-import { ErrorComponent } from './error/error.component';
-
 const routes: Routes = [
   {
     path: '',
