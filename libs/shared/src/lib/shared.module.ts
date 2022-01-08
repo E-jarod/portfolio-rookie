af3348363311aeb009a821cdfe38843f8ec42504
E-jarod <ejilane.jarod@gmail.com>
2022-01-09 00:15:59 +0100

    everything deleted

--- libs/shared/src/lib/shared.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FontawesomeModule } from './fontawesome/fontawesome.module';
-
-@NgModule({
-  imports: [CommonModule, FontawesomeModule],
-})
-export class SharedModule {}
--- libs/ui/src/lib/ui.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SkillBarModule } from './skill-bar/skill-bar.module';
-
-@NgModule({
-  imports: [CommonModule, SkillBarModule],
-})
-export class UiModule {}
